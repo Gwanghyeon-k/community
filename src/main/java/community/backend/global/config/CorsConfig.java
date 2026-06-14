a17913dbc2eb37e92571be3756e7cbed4aedf5e9
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2026-06-14 19:05:08 +0900

    [CHORE] cors 설정 수정

--- src/main/java/community/backend/global/config/CorsConfig.java
+++ src/main/java/community/backend/global/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins(
+            "http://localhost:3000",
+            "https://amumal-community.shop"
+        )
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .exposedHeaders("Authorization")
